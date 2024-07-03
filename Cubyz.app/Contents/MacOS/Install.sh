36b5d30c5ff767bcee04b89bc0f1d9fb07a911bb
Archbirdplus <archbirdplus@gmail.com>
2024-07-03 13:32:58 -0700

    Install: switch to curl instead of wget

--- Cubyz.app/Contents/MacOS/Install.sh
+++ Cubyz.app/Contents/MacOS/Install.sh
@@ -7,7 +7,7 @@ echo "Cubyz needs to install some dependencies."
 echo -n "Do you wish to install the dependency XQuartz? (yes/no) "
 read r
 case $r in
-    Yes | yes | y | Y ) break;;
+    Yes | yes | y | Y ) ;;
     * )
         echo "Not installing"
         echo "Press enter to close"
@@ -18,7 +18,7 @@ esac
 echo "Installing XQuartz."
 
 # XQuartz replaces all of brew install libxrandr libxrender libxdmcp libxau libxcb libxext libx11
-wget https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.8.5/XQuartz-2.8.5.pkg -v -O ../Resources/XQuartz-2.8.5.pkg && open ../Resources/XQuartz-2.8.5.pkg
+curl -L -o ../Resources/XQuartz-2.8.5.pkg https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.8.5/XQuartz-2.8.5.pkg && open ../Resources/XQuartz-2.8.5.pkg
 
 echo "Extra installation complete."
 echo "Press enter to continue..."
